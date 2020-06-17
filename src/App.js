4215b256bbd9ae24b2faaa488d3f4f51d032b28f
felipeegasneris <felipe.egas.neris@hotmail.com>
2020-06-17 16:12:52 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
+/** @jsx nativeEvents */
+import {useState} from 'react';
 import logo from './logo.svg';
+import nativeEvents from 'jsx-native-events'
 import './App.css';
 
 function App() {
+  const [ value, setValue ] = useState(12)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>el valor que llega desde el wc: {value}</p>
+        <my-counter start={value} onEventMyCounterAdd={e => setValue(e.detail.counter)}></my-counter>
       </header>
     </div>
   );
